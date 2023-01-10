30a34a9b59d240ca4e0ea56069a585acae6eb427
ADMIN <ADMIN@DESKTOP-6ULK7M6>
2023-01-10 13:08:33 +0530

    chaitu jenkins commit

---
+++ JenkinsProject/src/main/java/DemoJenkins/JenkinsTC1.java
@@ -0,0 +1,20 @@
+package DemoJenkins;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class JenkinsTC1 {
+	
+	@Test
+	public void m1() {
+		
+		Reporter.log("hello", true);
+		Reporter.log("helloBabu", true);
+		Reporter.log("gopi", true);
+	}
+	
+	
+
+}
